8f256784c798348edc01f30428e2bda16a0bc5b7
AlvinJK <ajk.alvin@gmail.com>
2021-11-16 14:55:08 +0700

    Setup db connection

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import logger from './logger';
-
 import createServer from './server';
+import db from './models';
+import logger from './logger';
 
 const port = process.env.PORT || 3000;
 
 (async () => {
   try {
     const app = createServer();
+    await db.startDB();
     app.listen(port, () => {
       logger.info(`Puppeteer Server has started! Listening on ${port}`);
     });
