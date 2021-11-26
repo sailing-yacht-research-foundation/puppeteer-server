32ff77529527fed1b25ca192bc398fc19df1fb3a
femi <femmie009@gmail.com>
2021-11-26 10:51:20 +0300

    made changes to ecr and ecs tf files

--- deployment/prod/ecr.tf
+++ deployment/prod/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "puppeteer_server_ecr" {
   name = "puppeteer-server"
 }
 
-data "aws_ecr_image" "puppeteer_service" {
-  repository_name = "puppeteer-server"
-  image_tag       = "v0.0.1"
-}
+#data "aws_ecr_image" "puppeteer_server" {
+#  repository_name = "puppeteer-server"
+#  image_tag       = "v0.0.1"
+#}
